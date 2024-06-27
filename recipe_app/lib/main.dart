23195d58670a1800300ddf757e012e46049921fb
yousefdewidar <ymahmoud1213@gmail.com>
2024-06-27 20:15:53 +0300

    Add image card to recipe details view

--- recipe_app/lib/main.dart
+++ recipe_app/lib/main.dart
@@ -13,17 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
-      ),
       debugShowCheckedModeBanner: false,
       routes: {
-        // :(context) => HomePage(),
         HomeView.id: (context) => const HomeView(),
         AllRecipesView.id: (context) => const AllRecipesView(),
         RecipeDetailsView.id: (context) => const RecipeDetailsView(),
       },
-      initialRoute: 'allRecipes',
+      initialRoute: RecipeDetailsView.id,
     );
   }
 }
--- recipe_app/lib/views/recipe_details_view.dart
+++ recipe_app/lib/views/recipe_details_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recips_app/widgets/details_view/image_food_details.dart';
 
 class RecipeDetailsView extends StatelessWidget {
   static String id = 'recipeDetails';
@@ -6,8 +7,34 @@ class RecipeDetailsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Burger Recipe",
+          style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+        ),
+        centerTitle: true,
+      ),
+      body: const Column(
+        children: [
+          ImageFoodCard(),
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Burger',
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+                
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
